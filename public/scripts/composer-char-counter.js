e1722f56efc48d8bac888303beb82b4248e2d923
Gagandeep Uppal <guppal.profile@gmail.com>
2021-10-05 20:57:15 +0000

    char counting done

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $('#tweet-text').on('focus blur keyup change paste', function() {
+
+    let tweetText = $(this).val();
+    let tweetLength = tweetText.length;
+    let charactersRemaining = 140 - tweetLength;
+		
+    $('output').text(charactersRemaining).css('color', charactersRemaining < 0 ? 'red' : '');
+
+  });
+});
